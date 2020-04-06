aee9fd2633d28ae2bb2469040a1d298e3802a022
Rodi Younies <govor_rodica@yahoo.com>
2020-04-06 17:19:17 +0300

    Clasa numerelor rationale si operatii cu acest tip de date.

---
+++ Operatii cu nr rationale/Operatii cu nr rationale/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatii_cu_nr_rationale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational r1 = new Rational();
+            r1.setNumarator(0);
+            r1.setNumitor(0);
+
+            Rational r2 = new Rational(4);
+            r2.setNumitor(0);
+
+            Rational r3 = new Rational(3,2);
+            Rational r4 = new Rational(1,2);
+            Rational r5 = r3.Add(r4);
+
+            Console.WriteLine($"{r3} + {r4} = {r5}");
+
+            Rational r6 = r3.Subtract(r4);
+            Console.WriteLine($"{r3} - {r4} = {r6}");
+
+            Rational r7 = r3.Multiply(r4);
+            Console.WriteLine($"{r3} * {r4} = {r7}");
+
+            Rational r8 = r3.Divide(r4);
+            Console.WriteLine($"{r3} / {r4} = {r8}");
+
+            int k = 3;
+            Rational r9 = r3.Power(k);
+            Console.WriteLine($"{r3} ^ {k} = {r9}");
+
+            Rational r10 = new Rational(1,2);
+            Rational r11 = new Rational(5,8);
+            if (r10 > r11)
+                Console.WriteLine("1/2 is greater than 5/8");
+            else
+                Console.WriteLine("1/2 is smaller than 5/8");
+
+            Rational r12 = new Rational(4, 8);
+            Rational r13 = new Rational(9, 18);
+            if (r12 == r13)
+                Console.WriteLine("4/8 is equal to 9/18");
+            else
+                Console.WriteLine("4/8 is not equal to 9/18");
+
+            Rational r14 = new Rational(4, 8);
+            Rational r15 = new Rational(9, 18);
+            if (r14 >= r15)
+                Console.WriteLine("4/8 is greater or equal to 9/18");
+            else
+                Console.WriteLine("4/8 is not greater or equal to 9/18");
+
+
+        }
+    }
+
+   
+}
---
+++ Operatii cu nr rationale/Operatii cu nr rationale/Rational.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+namespace Operatii_cu_nr_rationale
+{
+    internal class Rational
+    {
+        private int numarator;
+        private int numitor;
+
+        public object Util { get; private set; }
+
+        public Rational()
+        {
+            this.numarator =0;
+            this.numitor = 0;
+        }
+
+        public Rational(int numarator)
+        {
+            this.numarator = numarator;
+            this.numitor = 0;
+        }
+
+        public Rational (int numarator, int numitor)
+        {
+            this.numarator = numarator;
+            this.numitor = numitor;
+        }
+        public static bool operator == (Rational a, Rational b)
+        {
+            if(a.numarator*b.numitor ==  a.numitor*b.numarator)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public static bool operator != (Rational a, Rational b)
+        {
+            if (a.numarator * b.numitor == a.numitor * b.numarator)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool operator >(Rational a, Rational b)
+        {
+            if (a.numarator * b.numitor > a.numitor * b.numarator)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public static bool operator < (Rational a, Rational b)
+        {
+            if (a.numarator * b.numitor < a.numitor * b.numarator)
+            {
+                return true;
+            }
+
+            return false; ;
+        }
+
+        public static bool operator <= (Rational a, Rational b)
+        {
+            if (a.numarator * b.numitor <= a.numitor * b.numarator)
+            {
+                return true;
+            }
+
+            return false; ;
+        }
+        public static bool operator >=(Rational a, Rational b)
+        {
+            if (a.numarator * b.numitor >= a.numitor * b.numarator)
+            {
+                return true;
+            }
+
+            return false; ;
+        }
+        internal Rational Add(Rational r4)
+        {
+            Console.Write("Add--->");
+            Rational result = new Rational();
+
+            result.numitor = numitor * r4.numitor;
+            result.numarator = numarator * r4.numitor + r4.numarator * numitor;
+
+
+            return result.Reduce();
+        }
+
+        internal Rational Divide(Rational r4)
+        {
+            Console.Write("Divide--->");
+            return this.Multiply(r4.Inverse()).Reduce();
+        }
+
+        internal Rational Power(int k)
+        {
+            Console.Write("Power--->");
+            return new Rational((int)Math.Pow(numarator, k), (int)Math.Pow(numitor, k)).Reduce();
+        }
+
+        private Rational Inverse()
+        {
+            return new Rational(numitor, numarator);
+        }
+
+        internal Rational Multiply(Rational r4)
+        {
+            Console.Write("Multiply--->");
+            Rational result = new Rational();
+
+            result.numitor = numitor * r4.numitor;
+            result.numarator = numarator * r4.numitor;
+
+
+            return result.Reduce();
+        }
+
+        internal Rational Subtract(Rational r4)
+        {
+            Console.Write("Subtract--->");
+            Rational result = new Rational();
+
+            result.numitor = numitor * r4.numitor;
+            result.numarator = numarator * r4.numitor - r4.numarator * numitor;
+
+
+            return result.Reduce();
+        }
+
+        private Rational Reduce()
+        {
+            int d = Rational.gcd(Math.Abs(numarator), (numitor));
+
+            return new Rational(numarator / d, numitor / d);
+        }
+
+        public static int gcd(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            return a;
+        }
+
+        public void setNumarator(int numarator)
+        {
+            this.numarator = numarator;
+        }
+
+        public int getNumarator()
+        {
+            return this.numarator;
+        }
+
+        public void setNumitor(int numitor)
+        {
+            this.numitor = numitor;
+        }
+
+        public int getNumitor()
+        {
+            return this.numitor;
+        }
+        public override string ToString()
+        {
+            return string.Format("[{0} / {1}]", numarator, numitor);
+        }
+    }
+}
