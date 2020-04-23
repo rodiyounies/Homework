975f3efd1c77d4930afd909f8f00712ca133dd30
Rodi Younies <govor_rodica@yahoo.com>
2020-04-24 00:25:57 +0300

    Initial commit.

---
+++ Clasa Elevi/Clasa Elevi/Elev.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasa_Elevi
+{
+
+    class Elev: IComparable
+    {
+       
+
+        public Elev (string firstName, string lastName, double gradesArithmeticAverage)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.gradesArithmeticAverage = gradesArithmeticAverage;
+        }
+        
+        public string firstName {set; get; }
+        public string lastName {set; get; }
+        public double gradesArithmeticAverage { set; get; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            Elev elev1 = obj as Elev;
+            if (this.gradesArithmeticAverage == elev1.gradesArithmeticAverage)
+            {
+                int ret= elev1.firstName.CompareTo(this.firstName);
+                if (ret == 0)
+                {
+                    return elev1.lastName.CompareTo(this.lastName);
+
+                }
+                else
+                    return ret;
+            }
+           
+            else if (this.gradesArithmeticAverage > elev1.gradesArithmeticAverage)
+            {
+                return 1;
+            }
+            else return -1;
+
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName + " " + gradesArithmeticAverage;
+        }
+
+    }
+}
---
+++ Clasa Elevi/Clasa Elevi/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Clasa_Elevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Elev> elevi = new List<Elev>();
+            string[] lines = File.ReadAllLines("C:\\development\\strudents.txt");
+            foreach (string line in lines)
+            {
+                string[] tokens = line.Split(' ');
+                double gradesArithmeticAverage = 0;
+                int gradesCount = int.Parse(tokens[2]);
+
+                for (int i=0; i< gradesCount; i++)
+                {
+                    gradesArithmeticAverage += double.Parse(tokens[3 + i]);
+                }
+                gradesArithmeticAverage = gradesArithmeticAverage / gradesCount;
+                Elev v = new Elev(tokens[0], tokens[1], gradesArithmeticAverage);
+                elevi.Add(v);
+
+            }
+
+            Console.WriteLine("Elevi before sorting");
+            foreach (Elev v in elevi)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //sort students 
+            string temp_firstName;
+            string temp_lastName;
+            double temp_gradesArithmeticAverage;
+            for (int j = 0; j <= elevi.Count - 2; j++)
+            {
+                for (int i = 0; i <= elevi.Count - 2; i++)
+                {
+                    if (elevi.ElementAt(i).CompareTo(elevi.ElementAt(i+1)) < 0) 
+                    {
+                        temp_firstName = elevi.ElementAt(i+1).firstName;
+                        temp_lastName = elevi.ElementAt(i+1).lastName;
+                        temp_gradesArithmeticAverage = elevi.ElementAt(i+1).gradesArithmeticAverage;
+
+                        elevi.ElementAt(i+1).firstName = elevi.ElementAt(i).firstName;
+                        elevi.ElementAt(i+1).lastName = elevi.ElementAt(i).lastName;
+                        elevi.ElementAt(i+1).gradesArithmeticAverage = elevi.ElementAt(i).gradesArithmeticAverage;
+
+                        elevi.ElementAt(i).firstName = temp_firstName;
+                        elevi.ElementAt(i).lastName = temp_lastName;
+                        elevi.ElementAt(i).gradesArithmeticAverage = temp_gradesArithmeticAverage;
+                    }
+                }
+            }
+
+            Console.WriteLine("Elevi after sorting");
+            foreach (Elev v in elevi)
+            {
+                Console.WriteLine(v);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
