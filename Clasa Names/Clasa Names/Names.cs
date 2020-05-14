f3186a750cbe7913070cddca25f26c34107244e3
Rodi Younies <govor_rodica@yahoo.com>
2020-05-15 01:05:23 +0300

    Clasa Names cu indexatori de tip string si int pt tratarea exceptiilor.

---
+++ Clasa Names/Clasa Names/Names.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasa_Names
+{
+    class Names
+    {
+        private string[] names;
+        public int dimensiune;
+        public bool err;
+        public Names(List<string> values)
+        {
+            dimensiune = values.Count();
+            names = new string[values.Count()];
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = values.ElementAt(i);
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < dimensiune)
+                {
+                    err = false;
+                    return names[index];
+                }
+                else
+                {
+                    err = true;
+                    return index + ": Index out of range";
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < dimensiune)
+                {
+                    names[index] = value;
+                    err = false;
+                }
+                else
+                    err = true;
+            }
+        }
+        public string this[string index]
+        {
+            get
+            {
+                int i = 0;
+                try
+                {
+                    i=int.Parse(index);
+                }
+                catch(FormatException)
+                {
+                    err = true;
+                    return index + ": Number format exception!";
+                }
+                catch (ArgumentNullException)
+                {
+                    err = true;
+                    return index + ": Argument Null Exception!";
+                }
+                catch (OverflowException)
+                {
+                    err = true;
+                    return index + ": Overflow Exception!";
+                }
+
+                if (i >= 0 && i < dimensiune)
+                {
+                    err = false;
+                    return names[i];                       
+                }
+                else
+                {
+                    err = true;
+                    return i + ": Index out of range";
+                }
+            }
+        }
+    }
+}
---
+++ Clasa Names/Clasa Names/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasa_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> values = new List<string>();
+            values.Add("name1");
+            values.Add("name2");
+            values.Add("name3");
+            values.Add("name4");
+            values.Add("name5");
+
+            Names names = new Names(values);
+            string n1 = names[0];
+            Console.WriteLine(n1);
+
+            string n2 = names[10];
+            Console.WriteLine(n2);
+
+            string n3 = names["2"];
+            Console.WriteLine(n3);
+
+            string n4 = names["7"];
+            Console.WriteLine(n4);
+
+            string n5 = names["bla"];
+            Console.WriteLine(n5);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
