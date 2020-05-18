0e90cb480182aa15f4eef0ce7bf18d4a9a766c1e
Rodi Younies <govor_rodica@yahoo.com>
2020-05-18 13:58:23 +0300

    Clasa generica Stiva

---
+++ Clasa Stiva/Clasa Stiva/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasa_Stiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stiva cu valori int:");
+
+            Stiva<int> st = new Stiva<int>(4);
+            st.Push(1);
+            st.Push(2);
+            st.Push(3);
+            st.Push(4);
+
+           Console.WriteLine( st.Pop());
+           Console.WriteLine( st.Pop());
+           Console.WriteLine( st.Pop());
+           Console.WriteLine( st.Pop());
+           
+            Stiva<int> st1 = new Stiva<int>(4);
+            st1.Push(1);
+            st1.Push(2);
+            st1.Push(3);
+            st1.Push(4);
+
+           // st1.Clear();
+           // st1.Pop();
+
+            Console.WriteLine("Stiva cu valori string:");
+            Stiva<string> ststr = new Stiva<string>(4);
+            ststr.Push("1");
+            ststr.Push("2");
+            ststr.Push("3");
+            ststr.Push("4");
+
+           // ststr.Clear();
+            Console.WriteLine(ststr.Pop());
+            Console.WriteLine(ststr.Pop());
+            Console.WriteLine(ststr.Pop());
+            Console.WriteLine(ststr.Pop());
+            
+
+
+        }
+    }
+}
---
+++ Clasa Stiva/Clasa Stiva/StackFullException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Clasa_Stiva
+{
+    [Serializable]
+    internal class StackFullException : Exception
+    {
+        public StackFullException()
+        {
+        }
+
+        public StackFullException(string message) : base(message)
+        {
+        }
+
+        public StackFullException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StackFullException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
